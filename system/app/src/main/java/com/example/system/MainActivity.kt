0aba5ee782279df67b05807b565793299cd59ba6
awisu2 <awisu2@gmail.com>
2021-06-25 08:18:05 +0900

    SystemProjectを作成 Fixes #3

---
+++ system/app/src/main/java/com/example/system/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.system.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    // クラスに直接所属するSingleton(主に固定の変数などをセット)
+    companion object {
+        // constにするとstatic引数からの取得になる(つけない場合はget関数)
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            Log.d(TAG, "click!")
+
+            val intent = BuildActivity.createIntent(this)
+            startActivity(intent)
+        }
+    }
+}
