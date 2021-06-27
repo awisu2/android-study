8471fd2a3d9745f4b243d2c23d33e4a8fe660e41
awisu2 <awisu2@gmail.com>
2021-06-27 11:53:16 +0900

    #8 コメントの追加

--- layouts/app/src/main/java/com/example/layouts/DataBindingWithViewModelLiveDataActivity.kt
+++ layouts/app/src/main/java/com/example/layouts/DataBindingWithViewModelLiveDataActivity.kt
@@ -6,17 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.layouts.databinding.ActivityDataBindingWithViewModelLiveDataBinding
-import com.example.layouts.databinding.ActivityLinerLayoutBinding
 
 // DaatBindingと、ViewModel + LiveDataでの実装
 //
 // - viewModelに実装を記述することで
 //   - layoutの操作の記述をviewModelにまとめる事ができる
 //   - viewModelは複数記載できるため(もちろん煩雑になるが)特定の処理を分散させることができる
+//   - メモリリークが(viemodel, liveData による lifecycle の範囲において)考慮される
 // - databingindにより
 //   - idをベースとしたlayoutへの反映処理をロジックに載せずにすむ
+//   - `android:text="@{viewModel.text}"` といった変数的な記載が可能になる
 // - 双方向バインディングを設定することで
 //   - LiveData の observeを記載せずにすむ
+//
+// 双方向バインディングの実装の実装
+// - lifecycle の設定
+//   - `binding.lifecycleOwner = this`
+//   - fragmentの場合は `binding.lifecycleOwner = viewLifecycleOwner`
+// - layout.xmlへの記述
+//   - `android:text="@={viewModel.text}"`
+//     - `@=` と=をつけることで双方向になる
 //
 class DataBindingWithViewModelLiveDataActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDataBindingWithViewModelLiveDataBinding
