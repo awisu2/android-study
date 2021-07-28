e8d72be02da829b88eb1ab196d63e05d62af4bd1
awisu2 <awisu2@gmail.com>
2021-07-29 07:30:17 +0900

    dialogのサンプルを追加

--- dialog/app/src/main/java/com/example/dialog/MainActivity.kt
+++ dialog/app/src/main/java/com/example/dialog/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import com.example.dialog.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -20,6 +23,27 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             Log.d(TAG, "click")
+            dialog(it)
         }
+    }
+
+    private fun dialog(view: View) {
+        AlertDialog.Builder(this)
+            // 範囲外をクリックしたときにダイアログを消せるか？
+            .setCancelable(true)
+            .setMessage("メッセージ")
+            .setPositiveButton("OK", { dialog, which ->
+                showSnackbar(view, "clicked yes")
+            })
+            .setOnCancelListener {
+                showSnackbar(view, "canceled")
+            }
+            .show()
+        // alertdialogで処理は停止しないので、別途イベントをキャッチする必要
+        showSnackbar(view, "no wait daialog end")
+    }
+
+    private fun showSnackbar(view: View, message: String) {
+        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
     }
 }
